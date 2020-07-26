8ce55c94d7165a167191eefbdce458cc4d8ddbd6
smoewhere <648272510@qq.com>
2020-07-26 18:04:15 +0800

    update

---
+++ hibernate/src/main/java/org/fan/demo/hibernate/Main.java
@@ -0,0 +1,18 @@
+package org.fan.demo.hibernate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Fan
+ * @version 1.0
+ * @date 2020.7.26 15:49
+ */
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("hello world");
+    }
+}
