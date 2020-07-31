3530fb2c69565783eb3e77e017dc658096bea612
smoewhere <648272510@qq.com>
2020-07-31 14:48:58 +0800

    添加测试目录

---
+++ archetype/spring-mvc-1.1/src/test/java/org/fan/MainTest.java
@@ -0,0 +1,17 @@
+package org.fan;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @version 1.0
+ * @author: Fan
+ * @date 2020.7.31 14:43
+ */
+public class MainTest {
+
+  @Test
+  public void test() {
+    Assert.assertEquals(1, 1);
+  }
+}
