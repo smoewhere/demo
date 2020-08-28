7a6e6d3890e62bc7efb6c136847c02437d74bd63
smoewhere <648272510@qq.com>
2020-08-28 19:14:34 +0800

    添加filter

--- archetype/spring-mvc-1.1/src/main/java/org/fan/config/WebInitializer.java
+++ archetype/spring-mvc-1.1/src/main/java/org/fan/config/WebInitializer.java
@@ -1,11 +1,14 @@
 package org.fan.config;
 
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration.Dynamic;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /**
@@ -28,7 +31,11 @@ public class WebInitializer implements WebApplicationInitializer {
     DispatcherServlet servlet = new DispatcherServlet(context);
     ServletRegistration.Dynamic registration = servletContext.addServlet("app", servlet);
     registration.setLoadOnStartup(1);
-    registration.addMapping("/app/*");
+    registration.addMapping("/*");
+    // 添加filter
+    CharacterEncodingFilter filter = new CharacterEncodingFilter("UTF-8",true);
+    Dynamic filterRegistration = servletContext.addFilter("encodingFilter", filter);
+    filterRegistration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
 
   }
 }
