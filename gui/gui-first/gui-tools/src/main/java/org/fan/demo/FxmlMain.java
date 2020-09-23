95533fbb1e37840deee5ed79a66e0e72e1f03954
smoewhere <648272510@qq.com>
2020-09-23 22:42:22 +0800

    添加Fxml

---
+++ gui/gui-first/gui-tools/src/main/java/org/fan/demo/FxmlMain.java
@@ -0,0 +1,25 @@
+package org.fan.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Fan
+ * @version 1.0
+ * @date 2020.9.23 22:37
+ */
+public class FxmlMain extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
