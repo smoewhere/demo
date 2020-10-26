2f7bad464900eebe9461d0a9e2149339e9f2d4ca
smoewhere <648272510@qq.com>
2020-10-26 22:31:21 +0800

    移除用户登录缓存，正常情况下用户登录不需要缓存

--- security/security-test/src/main/java/org/fan/teat/security/config/security/UserConfig.java
+++ security/security-test/src/main/java/org/fan/teat/security/config/security/UserConfig.java
@@ -22,21 +22,12 @@ public class UserConfig implements UserDetailsService {
   @Resource
   private SysUserService sysUserService;
 
-  @Resource
-  private UserCache userCache;
-
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    UserDetails user = userCache.getUserFromCache(username);
-    if (user != null) {
-      return user;
-    }
     SysUser sysUser = sysUserService.selectAllByLoginName(username);
     if (sysUser == null) {
       throw new UsernameNotFoundException("user:{}" + username + "not found");
     }
-    user = new User(sysUser.getUserName(), sysUser.getPassword(), AuthorityUtils.createAuthorityList("1"));
-    userCache.putUserInCache(user);
-    return user;
+    return new User(sysUser.getUserName(), sysUser.getPassword(), AuthorityUtils.createAuthorityList("1"));
   }
 }
