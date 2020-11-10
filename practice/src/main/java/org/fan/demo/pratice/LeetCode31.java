804c2985802c4708ea4724ef7eb546241465260a
smoewhere <648272510@qq.com>
2020-11-10 18:06:05 +0800

    添加31题

---
+++ practice/src/main/java/org/fan/demo/pratice/LeetCode31.java
@@ -0,0 +1,49 @@
+package org.fan.demo.pratice;
+
+/**
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ * <p>
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * <p>
+ * 必须原地修改，只允许使用额外常数空间。
+ * <p>
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。 1,2,3 → 1,3,2 3,2,1 → 1,2,3 1,1,5 → 1,5,1
+ * <p>
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/next-permutation
+ *
+ * @version 1.0
+ * @author: Fan
+ * @date 2020.11.10 18:03
+ */
+public class LeetCode31 {
+
+  public void nextPermutation(int[] nums) {
+    int i = nums.length - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+      i--;
+    }
+    if (i >= 0) {
+      int j = nums.length - 1;
+      while (j >= 0 && nums[i] >= nums[j]) {
+        j--;
+      }
+      swap(nums, i, j);
+    }
+    reverse(nums, i + 1);
+  }
+
+  public void swap(int[] nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+
+  public void reverse(int[] nums, int start) {
+    int left = start, right = nums.length - 1;
+    while (left < right) {
+      swap(nums, left, right);
+      left++;
+      right--;
+    }
+  }
+}
