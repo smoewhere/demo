eb05cfabccc493e01687c3e4f865efd40c005a04
smoewhere <648272510@qq.com>
2020-11-22 21:53:15 +0800

    添加 242

---
+++ practice/src/main/java/org/fan/demo/pratice/simple/LeetCode242.java
@@ -0,0 +1,39 @@
+package org.fan.demo.pratice.simple;
+
+import java.util.Arrays;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 示例 1:
+ *
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ * 说明:
+ * 你可以假设字符串只包含小写字母。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-anagram
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author Fan
+ * @version 1.0
+ * @date 2020.11.22 21:51
+ */
+public class LeetCode242 {
+
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+    char[] str1 = s.toCharArray();
+    char[] str2 = t.toCharArray();
+    Arrays.sort(str1);
+    Arrays.sort(str2);
+    return Arrays.equals(str1, str2);
+  }
+
+}
