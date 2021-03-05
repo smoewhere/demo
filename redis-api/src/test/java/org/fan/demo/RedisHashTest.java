4a39ad5ef1f1bede01d1b75cd80c6055735e8ed2
smoewhere <648272510@qq.com>
2021-03-05 09:22:38 +0800

    update RedisHashTest.java

--- redis-api/src/test/java/org/fan/demo/RedisHashTest.java
+++ redis-api/src/test/java/org/fan/demo/RedisHashTest.java
@@ -1,8 +1,14 @@
 package org.fan.demo;
 
+import io.lettuce.core.MapScanCursor;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.ScanCursor;
 import io.lettuce.core.api.sync.RedisCommands;
+import java.text.MessageFormat;
+import java.util.Map;
+import java.util.StringJoiner;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,15 +28,16 @@ public class RedisHashTest {
 
   private static final RedisCommands<String, String> COMMANDS = REDIS_CLIENT.connect().sync();
 
-  private final String key = "test:hash:testHash";
+  private static final String key = "test:hash:testHash";
 
   @AfterAll
   static void afterAll() {
+    COMMANDS.del(key);
     REDIS_CLIENT.shutdown();
   }
 
-  @Test
-  public void testAdd(){
+  @BeforeAll
+  static void testAdd(){
     for (int i = 0; i < 200; i++) {
       COMMANDS.hset(key, "field" + i, "value" + i);
     }
@@ -38,11 +45,34 @@ public class RedisHashTest {
 
   @Test
   public void testHash(){
+    // 根据field获取key中的value
     String field0 = COMMANDS.hget(key, "field0");
     log.info("[RedisHashTest.testHash] hGet field0 is {}", field0);
+    // 类似setnx，如果不存在则设置value，如果存在则不进行操作
     Boolean result = COMMANDS.hsetnx(key, "field0", "value");
     log.info("[RedisHashTest.testHash] setnx result is {}", result);
+    // 获取hash的长度
+    Long len = COMMANDS.hlen(key);
+    log.info("[RedisHashTest.testHash] hash len is {}", len);
+    // 获取hash中filed里存储的字符串的长度
+    len = COMMANDS.hstrlen(key, "field0");
+    log.info("[RedisHashTest.testHash] field:{} 对应的value长度为{}", "field0",len);
 
+  }
+
+  /**
+   * scan的hash版本
+   */
+  @Test
+  public void testHashScan(){
+    MapScanCursor<String, String> scanCursor = new MapScanCursor<>();
+    scanCursor.setCursor("0");
+    do {
+      scanCursor = COMMANDS.hscan(key, scanCursor);
+      Map<String, String> result = scanCursor.getMap();
+      result.forEach((field, value)-> System.out.println(MessageFormat
+          .format("{0}:{1}",field, value)));
+    } while (!scanCursor.isFinished());
   }
 
 }
